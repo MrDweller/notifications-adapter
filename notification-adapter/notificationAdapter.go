55f931a0c5af12cb24d0cf649692c1a98fdc7a25
Jesper Frisk <jesper.frisk@hotmail.com>
2024-05-10 17:49:46 +0200

    changes from local event handler module to lib instead

--- notification-adapter/notificationAdapter.go
+++ notification-adapter/notificationAdapter.go
@@ -3,13 +3,13 @@ package notificationadapter
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/MrDweller/notificationadapter/event"
+	eventsubscriber "github.com/MrDweller/event-handler/subscriber"
+	eventhandlertypes "github.com/MrDweller/event-handler/types"
 	orchestratormodels "github.com/MrDweller/orchestrator-connection/models"
 	"github.com/MrDweller/orchestrator-connection/orchestrator"
 	"github.com/MrDweller/service-registry-connection/models"
@@ -21,8 +21,6 @@ type NotificationAdapter struct {
 	models.SystemDefinition
 	ServiceRegistryConnection serviceregistry.ServiceRegistryConnection
 	OrchestrationConnection   orchestrator.OrchestratorConnection
-	*event.Subscriber
-	eventChannel chan []byte
 
 	SystemAddress string
 	SystemPort    int
@@ -30,6 +28,7 @@ type NotificationAdapter struct {
 	NotificationUrl string
 
 	eventsToSubscribeTo []string
+	eventSubscriber     eventsubscriber.EventSubscriber
 }
 
 func NewNotificationAdapter(address string, port int, domainAddress string, domainPort int, systemName string, serviceRegistryAddress string, serviceRegistryPort int, notificationUrl string, eventsToSubscribeTo []string) (*NotificationAdapter, error) {
@@ -72,12 +71,26 @@ func NewNotificationAdapter(address string, port int, domainAddress string, doma
 		return nil, err
 	}
 
+	eventSubscriber, err := eventsubscriber.EventSubscriberFactory(
+		eventhandlertypes.EventHandlerImplementationType(os.Getenv("EVENT_HANDLER_IMPLEMENTATION")),
+		domainAddress,
+		domainPort,
+		systemName,
+		serviceRegistryAddress,
+		serviceRegistryPort,
+		serviceregistry.SERVICE_REGISTRY_ARROWHEAD_4_6_1,
+		os.Getenv("CERT_FILE_PATH"),
+		os.Getenv("KEY_FILE_PATH"),
+		os.Getenv("TRUSTSTORE_FILE_PATH"),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	notificationAdapter := &NotificationAdapter{
 		SystemDefinition:          systemDefinition,
 		ServiceRegistryConnection: serviceRegistryConnection,
 		OrchestrationConnection:   orchestrationConnection,
-		Subscriber:                event.NewSubscriber(),
-		eventChannel:              make(chan []byte),
 
 		SystemAddress: address,
 		SystemPort:    port,
@@ -85,6 +98,7 @@ func NewNotificationAdapter(address string, port int, domainAddress string, doma
 		NotificationUrl: notificationUrl,
 
 		eventsToSubscribeTo: eventsToSubscribeTo,
+		eventSubscriber:     eventSubscriber,
 	}
 	if err != nil {
 		return nil, err
@@ -93,61 +107,50 @@ func NewNotificationAdapter(address string, port int, domainAddress string, doma
 	return notificationAdapter, nil
 }
 
-func (notificationAdapter *NotificationAdapter) StartNotificationAdapter() error {
-	_, err := notificationAdapter.ServiceRegistryConnection.RegisterSystem(notificationAdapter.SystemDefinition)
+func (notificationAdapter *NotificationAdapter) ReceiveEvent(event []byte) {
+
+	var workEventDto WorkDTO
+	if err := json.Unmarshal(event, &workEventDto); err != nil {
+		log.Printf("[!] Error received event with unkown structure: %s\n", event)
+		return
+	}
+
+	log.Printf("[x] Received %s.\n", workEventDto)
+	err := notificationAdapter.HandleEvent(
+		WorkDTO{
+			EventType: workEventDto.EventType,
+			WorkId:    workEventDto.WorkId,
+			ProductId: workEventDto.ProductId,
+		},
+	)
 	if err != nil {
-		return err
+		log.Printf("[!] Error during handling of the event: %s\n", err)
+
 	}
+}
 
-	go func(eventsToSubscribeTo []string) {
-		for {
-			time.Sleep(4 * time.Second)
+func (notificationAdapter *NotificationAdapter) StartNotificationAdapter() error {
 
-			for _, event := range eventsToSubscribeTo {
-				err := notificationAdapter.Subscribe(event)
-				if err != nil {
-					log.Printf("\t[!] Error during subscription of event %s: %s\n", event, err)
+	for {
+		time.Sleep(4 * time.Second)
 
-				}
+		for _, event := range notificationAdapter.eventsToSubscribeTo {
+			err := notificationAdapter.Subscribe(event)
+			if err != nil {
+				log.Printf("\t[!] Error during subscription of event %s: %s\n", event, err)
+
 			}
-
 		}
 
-	}(notificationAdapter.eventsToSubscribeTo)
-
-	for receivedEvent := range notificationAdapter.eventChannel {
-		var event event.Event
-		if err := json.Unmarshal(receivedEvent, &event); err != nil {
-			log.Printf("\t[!] Error received event with unkown structure: %s\n", receivedEvent)
-			continue
-		}
-
-		log.Printf("\t[x] Received %s.\n", event)
-		err := notificationAdapter.HandleEvent(event)
-		if err != nil {
-			log.Printf("\t[!] Error during handling of the event: %s\n", err)
-
-		}
 	}
 
-	return nil
 }
 
 func (notificationAdapter *NotificationAdapter) StopNotificationAdapter() error {
-	err := notificationAdapter.Subscriber.UnsubscribeAll()
-	if err != nil {
-		return err
-	}
-
-	err = notificationAdapter.ServiceRegistryConnection.UnRegisterSystem(notificationAdapter.SystemDefinition)
-	if err != nil {
-		return err
-	}
-
-	return err
+	return notificationAdapter.eventSubscriber.UnregisterEventSubscriberSystem()
 }
 
-func (notificationAdapter *NotificationAdapter) HandleEvent(event event.Event) error {
+func (notificationAdapter *NotificationAdapter) HandleEvent(event WorkDTO) error {
 	notifyEventDTO := NotifyEventDTO{
 		ExternalSystemSlug: event.ProductId,
 		Type:               event.EventType,
@@ -166,72 +169,28 @@ func (notificationAdapter *NotificationAdapter) HandleEvent(event event.Event) e
 }
 
 func (notificationAdapter *NotificationAdapter) Subscribe(requestedService string) error {
-	orchestrationResponse, err := notificationAdapter.OrchestrationConnection.Orchestration(
-		requestedService,
-		[]string{
-			"AMQP-INSECURE-JSON",
-		},
-		orchestratormodels.SystemDefinition{
-			Address:    notificationAdapter.Address,
-			Port:       notificationAdapter.Port,
-			SystemName: notificationAdapter.SystemName,
-		},
-		orchestratormodels.AdditionalParametersArrowhead_4_6_1{
-			OrchestrationFlags: map[string]bool{
-				"overrideStore": true,
-			},
-		},
-	)
-	if err != nil {
-		return err
-	}
-
-	if len(orchestrationResponse.Response) <= 0 {
-		return errors.New("found no providers")
-	}
-	providers := orchestrationResponse.Response
-
-	for _, provider := range providers {
-		log.Printf("\t[*] Subscribing to %s events on %s at %s:%d.\n", requestedService, provider.Provider.SystemName, provider.Provider.Address, provider.Provider.Port)
-		go func(systemName string, address string, port int, serviceDefinition string, metadata map[string]string) {
-			err := notificationAdapter.Subscriber.Subscribe(
-				systemName,
-				address,
-				port,
-				event.EventDefinition{
-					EventType: serviceDefinition,
-				},
-				metadata,
-				notificationAdapter.eventChannel,
-			)
-
-			if err != nil {
-				log.Printf("\t[*] Error during subscription: %s\n", err)
-				return
-			}
-
-		}(provider.Provider.SystemName, provider.Provider.Address, provider.Provider.Port, requestedService, provider.Metadata)
-
-	}
-
-	return nil
+	log.Printf("[*] Subscribing to %s events.\n", requestedService)
+	return notificationAdapter.eventSubscriber.Subscribe(eventhandlertypes.EventType(requestedService), notificationAdapter)
 
 }
 
 func (notificationAdapter *NotificationAdapter) Unsubscribe(requestedService string) error {
-	err := notificationAdapter.Subscriber.UnsubscribeAllByEvent(
-		event.EventDefinition{
-			EventType: requestedService,
-		},
-	)
+	err := notificationAdapter.eventSubscriber.Unsubscribe(eventhandlertypes.EventType(requestedService))
 	if err != nil {
 		return err
 	}
-	log.Printf("\t[*] Unsubscribing from %s events.\n", requestedService)
+	log.Printf("[*] Unsubscribing from %s events.\n", requestedService)
 	return nil
 }
 
 type NotifyEventDTO struct {
 	ExternalSystemSlug string `json:"externalSystemSlug"`
 	Type               string `json:"type"`
+}
+
+type WorkDTO struct {
+	WorkId string `json:"workId"`
+
+	ProductId string `json:"productId"`
+	EventType string `json:"eventType"`
 }
